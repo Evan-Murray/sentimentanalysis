7ac8f1aa1f6068723893e0e70031a6c783347576
Evan-Murray <emurra9@gmail.com>
2018-05-03 14:03:17 -0400

    Update main.py

--- main.py
+++ main.py
@@ -25,4 +25,4 @@ def process():
     return jsonify({'error' : "Missing data. Ensure youve entered a valid search term."})
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', port=8080, debug=True)
+  app.run(host='0.0.0.0', port=5000, debug=True)
